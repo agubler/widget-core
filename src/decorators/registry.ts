9359e66c3871e67eebb6beeb9814d0bc7da92b9a
Anthony Gubler <agubler@sitepen.com>
2017-09-19 17:38:47 +0100

    update the jsdoc

--- src/decorators/registry.ts
+++ src/decorators/registry.ts
@@ -3,11 +3,14 @@ import { RegistryLabel, Render } from './../interfaces';
 import { RegistryItem } from './../Registry';
 import { handleDecorator, beforeRender, WidgetBase } from './../WidgetBase';
 
+/**
+ * Map of instances against registered item labels
+ */
 const instanceMap: WeakMap<WidgetBase, RegistryLabel[]> = new WeakMap();
 
 /**
  * Decorator function to registry items that will be defined against
- * the default registry of a widget.
+ * the local registry for a widget instance.
  *
  * @param label The label of the registry item
  * @param item The registry item to define
